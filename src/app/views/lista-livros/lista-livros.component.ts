d1b604d28c5f07c864b16378489f1b88197e96b8
tchfer <tchfernando@gmail.com>
2023-07-18 13:57:43 -0300

    Unsubscribe to service

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,21 +7,26 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   public listaLivros: [];
   public campoBusca: string = '';
+  private subscription: Subscription;
 
   constructor(
     private livroService : LivroService,
   ) { }
 
   buscarLivros() {
-    this.livroService.buscar(this.campoBusca).subscribe({
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
       next:(response) =>  console.log(response),
       error: (error) => console.error(error),
       complete: () => console.log('Observable completado') // Não aparece quando tem um erro porque o erro já encerra o ciclo
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 
