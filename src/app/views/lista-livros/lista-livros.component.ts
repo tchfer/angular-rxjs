7f9464c2d63a3bdf6e51ca80a66a0cecc780b749
tchfer <tchfernando@gmail.com>
2023-07-18 17:12:05 -0300

    Add error message

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { EMPTY, catchError, debounceTime, filter, map, switchMap, tap, throwError } from 'rxjs';
 
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
@@ -16,19 +16,28 @@ const PAUSA = 300; // const added to avoid magic numbers in application
 export class ListaLivrosComponent {
 
   public campoBusca = new FormControl();
+  mensagemErro: string = '';
 
   constructor(
     private livroService : LivroService,
   ) { }
 
-  livrosEncontrados$ = this.campoBusca.valueChanges
+  public livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
       debounceTime(PAUSA),
       filter(valorDigitado => valorDigitado.length >= 3),
       tap(() => console.log('Requisições ao servidor')),
       switchMap(valorDigitado => this.livroService.buscar(valorDigitado)),
       tap(() => console.log('Requisições ao servidor após o switchMap')),
-      map(items => this.livrosResultadoParaLivros(items))
+      map(items => this.livrosResultadoParaLivros(items)),
+      // catchError(erro => {
+      //   console.log(erro);
+      //   return throwError(() => new Error(this.mensagemErro = 'Ops! Ocorreu um erro. Recarregue a aplicação!'))
+      // })
+      catchError(() => {
+        this.mensagemErro = 'Ops! Ocorreu um erro. Recarregue a aplicação!';
+        return EMPTY; // A simple Observable that emits no items to the Observer and immediately emits a complete notification. That's why it needs to be reloaded. Cycle is complete
+      })
     )
 
   public livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
