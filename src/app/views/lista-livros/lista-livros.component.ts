4808da8e9c8a37854f0eec411822b6992768c6d7
tchfer <tchfernando@gmail.com>
2023-07-18 16:54:32 -0300

    Add debounceTime to improve search experience

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-import { filter, map, switchMap, tap } from 'rxjs';
+import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
 
 import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
+const PAUSA = 300; // const added to avoid magic numbers in application
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
@@ -22,6 +23,7 @@ export class ListaLivrosComponent {
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
+      debounceTime(PAUSA),
       filter(valorDigitado => valorDigitado.length >= 3),
       tap(() => console.log('Requisições ao servidor')),
       switchMap(valorDigitado => this.livroService.buscar(valorDigitado)),
