6902d0ec991697c0601fe80d4df41736b17d6836
tchfer <tchfernando@gmail.com>
2023-07-18 16:41:39 -0300

    Add observabe attribute and async pipe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { map, switchMap, tap } from 'rxjs';
+
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -9,26 +12,21 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent {
 
-  public listaLivros: Livro[];
-  public campoBusca: string = '';
-  public livro: Livro;
-  private subscription: Subscription;
+  public campoBusca = new FormControl();
 
   constructor(
     private livroService : LivroService,
   ) { }
 
-  public buscarLivros(): void {
-    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
-      next:(items) =>  {
-        this.listaLivros = this.livrosResultadoParaLivros(items);
-      },
-      error: (error) => console.error(error),
-      // complete: () => console.log('Observable completado') Não aparece quando tem um erro porque o erro já encerra o ciclo
-    });
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      tap(() => console.log('Requisições ao servidor')),
+      switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
+      tap(() => console.log('Requisições ao servidor após o switchMap')),
+      map(items => this.livrosResultadoParaLivros(items))
+    )
 
   public livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
@@ -36,7 +34,4 @@ export class ListaLivrosComponent implements OnDestroy {
     })
   }
 
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }
